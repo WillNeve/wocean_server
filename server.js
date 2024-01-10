b6c179baf9120d4e6ef1911d924d0d1559bab771
William Neve <williamneve6000@gmail.com>
2024-01-10 21:21:07 +0000

    test route added for front end editor development

--- server.js
+++ server.js
@@ -25,8 +25,20 @@ app.use('/test-route', (req, res) => {
   res.status(200).json({message: 'Deployed API working well :D'})
 })
 
+app.use('/note/:id', (req, res) => {
+  const responseObj = {
+    message: 'your request for note #2 was succesful',
+    page: {
+      title: 'A strong title',
+      body: '<div>Test</div><div>Test</div><div>sdfsdf</div><div>sdf</div><div>sdf</div><div>sd</div><div>fs</div><div>df</div><div>sd</div><div>f</div><div>sd</div><div>f</div><div>------------</div><p>some text</p>'
+    }
+  }
+  res.status(200).json(responseObj)
+})
+
 app.use('/', userRoutes)
 // ----------------
+
 
 // ---- Start listening for requests --------
 app.listen(port, () => {
