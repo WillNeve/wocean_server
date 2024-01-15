22c4e17f0f9fae80ca00504c6518412eba29dba1
William Neve <williamneve6000@gmail.com>
2024-01-15 17:45:37 +0000

    added test note route for offline dev

--- routes/notes.js
+++ routes/notes.js
@@ -7,6 +7,11 @@ router.get('/notes/new', isUserAuthenticated, notesController.create)
 
 router.get('/user/:user_id/notes', isUserAuthorized, notesController.retrieveAll)
 
+router.get('/notes/test', (req, res) => {
+  console.log('request for test note recieved');
+  res.status(200).json({message: 'test note for offline dev',
+                            note: {title: 'Test note', body: '[{"id": 0, "type": "p", "content": "test line"}]'}})
+})
 router.get('/notes/:note_id', notesController.getUserIdFromNote, isUserAuthorized, notesController.retrieve)
 
 router.patch('/notes/:note_id', notesController.getUserIdFromNote, isUserAuthorized, notesController.persist)
