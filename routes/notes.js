5b47d2c9d5a5a1caffc9963000654ea417e6a04d
William Neve <williamneve6000@gmail.com>
2024-01-15 18:27:52 +0000

    added test routes for dashboard notes index

--- routes/notes.js
+++ routes/notes.js
@@ -5,13 +5,22 @@ const { isUserAuthorized, isUserAuthenticated } = require('../controllers/auth')
 
 router.get('/notes/new', isUserAuthenticated, notesController.create)
 
+// -------- For Offline Testing ------------
+
+// router.get('/user/:user_id/notes', (req, res) => {
+//   res.status(200).json({notes: [{title: 'Test note', body: '[{"id": 0, "type": "p", "content": "test line"}]'},{title: 'Test note 2', body: '[{"id": 0, "type": "p", "content": "test line"}]'}]})
+// })
+
+// router.get('/notes/test', (req, res) => {
+//   console.log('request for test note recieved');
+//   res.status(200).json({message: 'test note for offline dev',
+//                             note: {title: 'Test note', body: '[{"id": 0, "type": "p", "content": "test line"}]'}})
+// })
+
+// -----------
+
 router.get('/user/:user_id/notes', isUserAuthorized, notesController.retrieveAll)
 
-router.get('/notes/test', (req, res) => {
-  console.log('request for test note recieved');
-  res.status(200).json({message: 'test note for offline dev',
-                            note: {title: 'Test note', body: '[{"id": 0, "type": "p", "content": "test line"}]'}})
-})
 router.get('/notes/:note_id', notesController.getUserIdFromNote, isUserAuthorized, notesController.retrieve)
 
 router.patch('/notes/:note_id', notesController.getUserIdFromNote, isUserAuthorized, notesController.persist)
